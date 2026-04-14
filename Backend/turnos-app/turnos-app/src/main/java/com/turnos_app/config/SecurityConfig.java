8eaaa855af42dd1ca06dec307ebcfb3efb365f29
VargasSebastian99 <guss666important@gmail.com>
2026-04-14 17:27:51 -0300

    reorganización de carpetas

--- Backend/turnos-app/turnos-app/src/main/java/com/turnos_app/config/SecurityConfig.java
+++ Backend/turnos-app/turnos-app/src/main/java/com/turnos_app/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                         .requestMatchers("/auth/login").permitAll()
+                         .requestMatchers("/auth/login", "/auth/me").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
